15e3aefdf0efbe02e51ec197e8e895da8f1d9723
Lucca Torrezilha Soares <68205690+SrLucca@users.noreply.github.com>
2020-09-22 11:36:32 -0300

    edit-js-search

--- teste_oficina/teste_app/urls.py
+++ teste_oficina/teste_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('index.html/', views.Page, name='index'),
     path('post/<int:id>', views.Postview, name='postview'),
     path('about.html/', views.About, name='about'),
+    path('portifolio.html/', views.Portifolio, name='portifolio')
 ]
--- teste_oficina/teste_app/views.py
+++ teste_oficina/teste_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 from .models import Post
 # Create your views here.
@@ -10,9 +11,22 @@ def Base(request):
 def About(request):
     return render(request, 'paginas/about.html')
 
+def Portifolio(request):
+    return render(request, 'paginas/portifolio.html')
+
 def Page(request):
-    posts = Post.objects.all().order_by('-created_at')
+    search = request.GET.get('search')
+
+    if search:
+        posts = Post.objects.filter(title__icontains=search) 
+    else:
+        posts_list = Post.objects.all().order_by('-created_at') #mostar os posts
+        paginator = Paginator(posts_list, 3) #Dê ao Paginator uma lista de objetos, mais o número de itens que você gostaria de ter em cada página, e ele fornece métodos para acessar os itens de cada página
+        page = request.GET.get('page') #tentando alcançar aqui é encontrar o valor para a chave 'página' fornecida
+        posts = paginator.get_page(page) # "refresh" na pagina com o resultado da busca
+
     return render(request, 'paginas/index.html', {'posts': posts})
+
 
 def Postview(request, id):
     post = get_object_or_404(Post, pk=id)#recebe o model e o id
