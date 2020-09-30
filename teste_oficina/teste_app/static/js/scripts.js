3dd523bfb5464c1d3170e7d686d2679319539e3a
Lucca Torrezilha Soares <68205690+SrLucca@users.noreply.github.com>
2020-09-30 02:35:19 -0300

    final-edit

--- teste_oficina/teste_app/static/js/scripts.js
+++ teste_oficina/teste_app/static/js/scripts.js
@@ -2,9 +2,10 @@ $(document).ready(function(){ /*ao abrir o documento(pagina) esta função sera 
         var searchBtn = $('#search-btn');   /*$ = atalho para jQuery = biblioteca de funções javascript que interagem com html*/
         var searchForm = $('#search-form'); /*$ = identificador valido de java script */
 
-
+        
 
         $(searchBtn).on('click', function(){
             searchForm.submit();
         });
+
 });
